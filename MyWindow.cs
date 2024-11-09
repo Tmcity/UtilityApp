2939afd998e1f7acf5194305cb0f568c7cd338d3
Tmcity <tmcity233@gmail.com>
2024-11-09 15:24:00 +0800

    初始化项目,使用 WinFormium
    - 在 `App.config` 文件中添加了多个程序集绑定重定向配置。
    - 移除了 `Form1.Designer.cs` 和 `Form1.cs` 文件。
    - 在 `Program.cs` 文件中，移除了原有的 `Main` 方法，改为使用 `WinFormium` 库来创建和运行应用程序。
    - 更新 `README.md` 。
    - 更新 SDK ，并添加了多个 NuGet 包引用。
    - 新增 `MyApp.cs` 文件，配置 `WinFormium` 应用程序的初始化信息。
    - 新增 `MyWindow.cs` 文件，配置应用程序的主窗体。

---
+++ MyWindow.cs
@@ -0,0 +1,24 @@
+﻿using WinFormium;
+using WinFormium.Forms;
+
+class MyWindow : Formium
+{
+    public MyWindow()
+    {
+        // 设置主页地址
+        Url = "https://cloud.tmcity233.com";
+    }
+
+    protected override FormStyle ConfigureWindowStyle(WindowStyleBuilder builder)
+    {
+        // 此处配置窗体的样式和属性，或不继承此方法以使用默认样式。
+
+        var style = builder.UseSystemForm();
+
+        style.TitleBar = true;
+
+        style.DefaultAppTitle = "天幕の云盘";
+
+        return style;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,22 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using WinFormium;
 
-namespace UtilityApp
+class Program
 {
-    internal static class Program
+    [STAThread]
+    // Main函数中的代码应该在这里，该函数只在主进程中运行。这样可以防止子进程运行一些不正确的初始化代码。
+    static void Main(string[] args)
     {
-        /// <summary>
-        /// 应用程序的主入口点。
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
+        // 创建WinFormiumApp的构建器
+        var builder = WinFormiumApp.CreateBuilder();
+
+        // 使用MyApp类作为应用程序的入口点
+        builder.UseWinFormiumApp<MyApp>();
+
+        // 构建应用程序
+        var app = builder.Build();
+
+        // 运行应用程序
+        app.Run();
     }
 }
